1050caea4d24e05cf5fcf797181c83fc2d555533
oshry <oshrylevy@gmail.com>
2014-12-22 11:37:13 +0200

    erere

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+          function ajax(data,type, datatype){
+              $.ajax({
+                    url: "sort.php",
+                    data: data,
+                    type: type,
+                    dataType: datatype,
+                    success: function(res) {
+                        var tmpl = $("#matches_tmpl").html();
+                        var html = Mustache.to_html(tmpl, res);
+                        $("#data").html(html);
+                    }
+              });
+          }  
+          var data = {matchType: 0};
+          ajax(data, "post", "json");
+          $("input[type=radio]").change(function(){
+                  var matchType = $("input[type=radio]:checked").attr("value");
+                  var data = {matchType: matchType};
+                  ajax(data, "post", "json");  
+          });
+        $("#car_form").submit(function(event){
+            event.preventDefault();
+            
+        });
+ });
