d14589946f6919108d0de409c68c7ff77fa49682
oshry <oshrylevy@gmail.com>
2014-12-27 10:58:42 +0200

    end of basic rest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,48 @@
 $(document).ready(function() {
-          function ajax(data,type, datatype){
-              $.ajax({
-                    url: "sort.php",
-                    data: data,
-                    type: type,
-                    dataType: datatype,
-                    success: function(res) {
-                        var tmpl = $("#matches_tmpl").html();
-                        var html = Mustache.to_html(tmpl, res);
-                        $("#data").html(html);
-                    }
-              });
-          }  
-          var data = {matchType: 0};
-          ajax(data, "post", "json");
-          $("input[type=radio]").change(function(){
-                  var matchType = $("input[type=radio]:checked").attr("value");
-                  var data = {matchType: matchType};
-                  ajax(data, "post", "json");  
-          });
         $("#car_form").submit(function(event){
             event.preventDefault();
-            
+            var data = $(this).serialize();
+            $.ajax({
+                url: "cars",
+                data: data,
+                type: "post",
+                success: function(res) {
+                    location.reload();
+                }
+            });
+        });
+        $(".car_edit").submit(function(event){
+            event.preventDefault();
+            id = $(this).attr('id');
+            id = id.replace("car_edit", "");
+            var data = $(this).serialize();
+            $.ajax({
+                url: "cars/"+id,
+                data: data,
+                type: "put",
+                success: function(res) {
+                    location.reload();
+                }
+            });
+        });
+        $(".delete").click(function(e){
+            var id = $(this).attr('id');
+            id = id.replace("delete", "");
+            $.ajax({
+                url: "cars/"+id,
+                type:"delete",
+                success: function(res){
+                    location.reload();
+                    //alert("Car Number :"+$(this).attr('id')+" Deleted");
+                }
+            });
+
+        });
+        $('.edit').click(function(){
+            var id = $(this).attr('id');
+            id = id.replace("edit", "");
+            $("#toggle"+id).toggle( "slow", function() {
+                // Animation complete.
+             });
         });
  });
